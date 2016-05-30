ff4c61502a00d741d554ad9057b35e4b6e46152d
Popoola Oluwaseun <seunpopoola@outlook.com>
2016-05-30 15:28:19 +0100

    Create a service interface with the implementation class.

---
+++ BlogDomainProject/Logic/DomainService.cs
@@ -0,0 +1,15 @@
+﻿using BlogDomainProject.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDomainProject.Logic
+{
+    public class DomainService : IBlogDomain
+    {
+
+        
+    }
+}
